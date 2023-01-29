315b17824b6e40100abadba398732bb35f0d263c
botlent <valencopercop@gmail.com>
2023-01-29 23:48:41 +0700

    kontol

---
+++ settings.js
@@ -0,0 +1,70 @@
+
+const fs = require('fs')
+const chalk = require('chalk')
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+//==============================\
+
+global.ownerlen = "RUBAH AJA MAKE NAMAMU"
+global.owner = ['628nomormu'] 
+global.ownername = "maslentRubahAjaNamamu"
+global.ytname = "YT: lenttobs"
+global.socialm = "IG: vlntncptr"
+global.location = "Indonesia, Jawatimur, Lumajang"
+global.ownernomer = "62xxxxNomormu"
+global.premium = ['628xxnomormu']
+global.botname = 'mzlntNamamu' 
+global.linkz = "https://chat.LINK BOKEP/LINKGC WA"
+global.websitex = "yt mu"
+global.botscript = '-'
+global.themeemoji = "😿"
+global.packname = "Sticker By"
+global.author = "Made by"
+global.wm = "Lenttobs"
+global.sessionName = 'session'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: 'Piye?!', //ubah aja make bhs indo terserahhh karepmu
+    admin: 'This feature could be used by admins only!',
+    botAdmin: 'Bot Must Be Admin First!',
+    premime: 'Premium Special Features If You Want to Register Type Rent',
+    owner: 'This feature could be used by owner only',
+    group: 'Features Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This feature could be used by bot only',
+    wait: 'In process...',
+    linkm: 'Where is the link?',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+}
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.thum = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.log0 = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.err4r = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+global.thumb = fs.readFileSync("./Maslent/pplent/maslent.jpg")
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12, // limit awall 
+    monayawal: 1000
+}
+global.limitAwal = {
+ prem: 'Unlimited',
+ free: 70
+}
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
